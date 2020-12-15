3e66fe2797f1b5c97ee3d614512c44e8ead1f669
kwsong <kwsong@berkeley.edu>
2020-12-15 09:30:19 -0800

    Add files via upload

--- UnmakingBulge.py
+++ UnmakingBulge.py
@@ -11,7 +11,7 @@ import System
 from System.Threading import Tasks as tasks
 from System.Collections.Generic import List
 
-def DoSomething():
+def Bulge():
 
     input_mesh_id = rs.GetObjects("Select model mesh", 32, True, False)
     if not input_mesh_id: return
@@ -24,8 +24,6 @@ def DoSomething():
 
     input_mesh = rs.coercemesh(input_mesh_id, True)
     offset_mesh = input_mesh.Offset(0.5)
-    #scriptcontext.doc.Objects.AddMesh(offset_mesh)
-    #input_brep = Rhino.Geometry.Brep.CreateFromMesh(input_mesh, False)
     input_brep = Rhino.Geometry.Brep.CreateFromMesh(offset_mesh, False)
 
     cutter_mesh = rs.coercemesh(cutter_mesh_id, True)
@@ -34,12 +32,10 @@ def DoSomething():
     cutter_brep = Rhino.Geometry.Brep.CreateFromMesh(offset_cutter_meshes, False)
 
     brep_pieces = Rhino.Geometry.Brep.Split(input_brep, cutter_brep, 0.01)
-    inner_chamber_brep = brep_pieces[2]
-    scriptcontext.doc.Objects.AddBrep(brep_pieces[2])
-    #inner_chamber = Rhino.Geometry.Mesh.CreateFromBrep(brep_pieces[2])
-    #scriptcontext.doc.Objects.AddMesh(inner_chamber)
+    inner_chamber_brep = brep_pieces[2] # may need to manually change this index for some models/cuts
+    scriptcontext.doc.Objects.AddBrep(inner_chamber_brep)
+
     inner_chamber_mesh = Rhino.Geometry.Mesh()
-    #inner_chamber = Rhino.Geometry.Brep.CreateOffsetBrep(brep_pieces[2], -0.5, False, False, 0.1)
     mesh_array = Rhino.Geometry.Mesh.CreateFromBrep(inner_chamber_brep)
     for mesh in mesh_array:
         inner_chamber_mesh.Append(mesh)
@@ -49,13 +45,9 @@ def DoSomething():
     inner_chamber_mesh.Append(input_mesh)
     scriptcontext.doc.Objects.AddMesh(inner_chamber_mesh) # main material mesh
 
-    #scriptcontext.doc.Objects.AddBrep(input_brep)
-    #scriptcontext.doc.Objects.AddBrep(cutter_brep)
-    #scriptcontext.doc.Objects.AddBrep(inner_chamber[0])
-    #scriptcontext.doc.Objects.AddBrep(inner_chamber[0])
     #for brep_piece in inner_chamber[0]:
     #    scriptcontext.doc.Objects.AddBrep(brep_piece)
       #doc.Objects.Delete(obj_ref, False)
     scriptcontext.doc.Views.Redraw()
     return Rhino.Commands.Result.Success
-DoSomething()
+Bulge()
